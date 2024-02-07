091ae166be76f1841e7126b37a13f24d87c7f3d1
Jorge Ramalhete <jorgeramalhete.work@gmail.com>
2024-02-07 19:03:05 +0000

    update

--- Project/assets/dev/3d.js
+++ Project/assets/dev/3d.js
@@ -14,7 +14,7 @@ camara.lookAt(0, 0, 0);
 /* renderer... */
 var canvas = document.getElementById("display_content");
 let renderer = new THREE.WebGLRenderer({ canvas: canvas, antialias: true, alpha: true });
-renderer.setSize(520, 400);
+renderer.setSize(520, 460);
 renderer.toneMapping = THREE.ReinhardToneMapping;
 renderer.toneMappingExposure = 1.7;
 renderer.shadowMap.enabled = true;
@@ -30,19 +30,75 @@ var lightHolder = new THREE.Group();
 lightHolder.add(pointLight);
 cena.add(lightHolder);
 
+var misturador = new THREE.AnimationMixer(cena)
+
+/*********************************************LOAD TEXTURES********************************************/
 var wood_normal_texture = new THREE.TextureLoader().load('assets/materials/Wood_Normal_2K.png');
 wood_normal_texture.wrapS = THREE.RepeatWrapping;
 wood_normal_texture.wrapT = THREE.RepeatWrapping;
 wood_normal_texture.magFilter = THREE.NearestFilter;
 wood_normal_texture.rotation = 1.571;
 wood_normal_texture.repeat.set(4, 4);
-let wood_material = new THREE.MeshBasicMaterial({
+let wood_normal_material = new THREE.MeshBasicMaterial({
     map: wood_normal_texture,
     side: THREE.DoubleSide,
 });
 
-var misturador = new THREE.AnimationMixer(cena)
+var wood_black_texture = new THREE.TextureLoader().load('assets/materials/Wood_Black_2K.png');
+wood_black_texture.wrapS = THREE.RepeatWrapping;
+wood_black_texture.wrapT = THREE.RepeatWrapping;
+wood_black_texture.magFilter = THREE.NearestFilter;
+wood_black_texture.rotation = 1.571;
+wood_black_texture.repeat.set(4, 4);
+let wood_black_material = new THREE.MeshBasicMaterial({
+    map: wood_black_texture,
+    side: THREE.DoubleSide,
+});
 
+var wood_gray_texture = new THREE.TextureLoader().load('assets/materials/Wood_Gray_2K.png');
+wood_gray_texture.wrapS = THREE.RepeatWrapping;
+wood_gray_texture.wrapT = THREE.RepeatWrapping;
+wood_gray_texture.magFilter = THREE.NearestFilter;
+wood_gray_texture.rotation = 1.571;
+wood_gray_texture.repeat.set(4, 4);
+let wood_gray_material = new THREE.MeshBasicMaterial({
+    map: wood_gray_texture,
+    side: THREE.DoubleSide,
+});
+
+var wood_blue_texture = new THREE.TextureLoader().load('assets/materials/Wood_Blue_2K.png');
+wood_blue_texture.wrapS = THREE.RepeatWrapping;
+wood_blue_texture.wrapT = THREE.RepeatWrapping;
+wood_blue_texture.magFilter = THREE.NearestFilter;
+wood_blue_texture.rotation = 1.571;
+wood_blue_texture.repeat.set(4, 4);
+let wood_blue_material = new THREE.MeshBasicMaterial({
+    map: wood_blue_texture,
+    side: THREE.DoubleSide,
+});
+
+var wood_red_texture = new THREE.TextureLoader().load('assets/materials/Wood_Red_2K.png');
+wood_red_texture.wrapS = THREE.RepeatWrapping;
+wood_red_texture.wrapT = THREE.RepeatWrapping;
+wood_red_texture.magFilter = THREE.NearestFilter;
+wood_red_texture.rotation = 1.571;
+wood_red_texture.repeat.set(4, 4);
+let wood_red_material = new THREE.MeshBasicMaterial({
+    map: wood_red_texture,
+    side: THREE.DoubleSide,
+});
+
+var wood_green_texture = new THREE.TextureLoader().load('assets/materials/Wood_Green_2K.png');
+wood_green_texture.wrapS = THREE.RepeatWrapping;
+wood_green_texture.wrapT = THREE.RepeatWrapping;
+wood_green_texture.magFilter = THREE.NearestFilter;
+wood_green_texture.rotation = 1.571;
+wood_green_texture.repeat.set(4, 4);
+let wood_green_material = new THREE.MeshBasicMaterial({
+    map: wood_green_texture,
+    side: THREE.DoubleSide,
+});
+/******************************************END LOAD TEXTURES********************************************/
 
 function render() {
     renderer.render(cena, camara);
@@ -53,17 +109,17 @@ function render() {
 let loader = new GLTFLoader();
 let model;
 let gaveta_baixo,
-gaveta_meio,
-gaveta_cima,
-porta_direita,
-porta_meio,
-porta_esquerda;
+    gaveta_meio,
+    gaveta_cima,
+    porta_direita,
+    porta_meio,
+    porta_esquerda;
 let open_gaveta_baixo,
-open_gaveta_meio,
-open_gaveta_cima,
-open_porta_direita,
-open_porta_meio,
-open_porta_esquerda;
+    open_gaveta_meio,
+    open_gaveta_cima,
+    open_porta_direita,
+    open_porta_meio,
+    open_porta_esquerda;
 
 loader.load('model/aparador.gltf',
     function (gltf) {
@@ -103,6 +159,7 @@ loader.load('model/aparador.gltf',
                 open_porta_esquerda = misturador.clipAction(animOpenPE);
             }
         })
+        setInitialColor()
     }
 )
 
@@ -193,7 +250,7 @@ function playGavetas() {
     playGavetaCima()
 }
 
-function playPortaDireita(){
+function playPortaDireita() {
     if (open_porta_direita.paused == true) {
         open_porta_direita.paused = false;
         open_porta_direita.enabled = true;
@@ -204,8 +261,8 @@ function playPortaDireita(){
     open_porta_direita.clampWhenFinished = true;
 }
 
-function playPortaMeio(){
-    
+function playPortaMeio() {
+
     if (open_porta_meio.paused == true) {
         open_porta_meio.paused = false;
         open_porta_meio.enabled = true;
@@ -216,7 +273,7 @@ function playPortaMeio(){
     open_porta_meio.clampWhenFinished = true;
 }
 
-function playPortaEsquerda(){
+function playPortaEsquerda() {
     if (open_porta_esquerda.paused == true) {
         open_porta_esquerda.paused = false;
         open_porta_esquerda.enabled = true;
@@ -230,28 +287,119 @@ function playPortaEsquerda(){
 function playPortas() {
     playPortaDireita()
     playPortaMeio()
-    playPortaEsquerda()    
+    playPortaEsquerda()
 }
 
+
+let nodesToChange =
+    ['Aparador', 'gaveta_baixo_material', 'gaveta_meio_material',
+        'gaveta_cima_material', 'porta_esquerda_material', 'porta_meio_material',
+        'porta_direita_material'];
+
+function setInitialColor() {
+    nodesToChange.forEach(nodeName => {
+        const object = model.getObjectByName(nodeName, true);
+        if (object) {
+            object.traverse(child => {
+                if (child.isMesh) {
+                    // Set the initial material here
+                    child.material = wood_normal_material;
+                }
+            });
+        }
+    });
+    render(); // Assuming you have a render function
+}
+
+let select_secretary_img = document.getElementById('select_secretary_img')
 function changeColor() {
     const colorSelector = document.getElementById('colorSelector');
     const selectedColor = colorSelector.value;
-
     switch (selectedColor) {
+        case 'normal':
+            nodesToChange.forEach(nodeName => {
+                const object = model.getObjectByName(nodeName, true); // Recursive search
+                if (object) {
+                    object.traverse(child => {
+                        if (child.isMesh) {
+                            child.material = wood_normal_material;
+                            select_secretary_img.src = "./assets/product-page/desk.png";
+                        }
+                    });
+                }
+            });
+            break;
+        case 'black':
+            nodesToChange.forEach(nodeName => {
+                const object = model.getObjectByName(nodeName, true); // Recursive search
+                if (object) {
+                    object.traverse(child => {
+                        if (child.isMesh) {
+                            child.material = wood_black_material;
+                            select_secretary_img.src = "./assets/product-page/desk_black.png";
+                        }
+                    });
+                }
+            });
+            break;
+        case 'gray':
+            nodesToChange.forEach(nodeName => {
+                const object = model.getObjectByName(nodeName, true); // Recursive search
+                if (object) {
+                    object.traverse(child => {
+                        if (child.isMesh) {
+                            child.material = wood_gray_material;
+                            select_secretary_img.src = "./assets/product-page/desk_gray.png";
+                        }
+                    });
+                }
+            });
+            break;
         case 'blue':
-            // Traverse through all the model's children and change their material color to blue
-            model.traverse(child => {
-                if (child.isMesh) {
-                    
+            nodesToChange.forEach(nodeName => {
+                const object = model.getObjectByName(nodeName, true); // Recursive search
+                if (object) {
+                    object.traverse(child => {
+                        if (child.isMesh) {
+                            child.material = wood_blue_material;
+                            select_secretary_img.src = "./assets/product-page/desk_blue.png";
+                        }
+                    });
+                }
+            });
+            break;
+        case 'red':
+            nodesToChange.forEach(nodeName => {
+                const object = model.getObjectByName(nodeName, true); // Recursive search
+                if (object) {
+                    object.traverse(child => {
+                        if (child.isMesh) {
+                            child.material = wood_red_material;
+                            select_secretary_img.src = "./assets/product-page/desk_red.png";
+                        }
+                    });
+                }
+            });
+            break;
+        case 'green':
+            nodesToChange.forEach(nodeName => {
+                const object = model.getObjectByName(nodeName, true); // Recursive search
+                if (object) {
+                    object.traverse(child => {
+                        if (child.isMesh) {
+                            child.material = wood_green_material;
+                            select_secretary_img.src = "./assets/product-page/desk_green.png";
+                        }
+                    });
                 }
             });
             break;
         default:
             break;
     }
-
-    // Ensure to update the scene to reflect the changes
+    render()
 }
+
 
 
 function changeSize() {
@@ -260,13 +408,13 @@ function changeSize() {
 
     switch (selectedSize) {
         case 'size_M':
-            model.scale.set(1, 1, 1.1);
+            model.scale.set(1.1, 1, 1);
             break;
         case 'size_L':
-            model.scale.set(1, 1, 1.2);
+            model.scale.set(1.2, 1, 1);
             break;
         case 'size_XL':
-            model.scale.set(1, 1, 1.3);
+            model.scale.set(1.3, 1, 1);
             break;
         default:
             model.scale.set(1, 1, 1);
@@ -295,17 +443,60 @@ function move_model_z() {
     render();
 }
 
+let isAnimatingCamera;
+var cameraTarget = new THREE.Vector3(0, 0, 0);
+var lerpValue = 0.05;
+let clickCount = 0;
+
+function animateCamera() {
+    if (!isAnimatingCamera) return;
+    requestAnimationFrame(animateCamera);
+    if (camara.position.distanceTo(cameraTarget) > 0.01) {
+        camara.position.lerp(cameraTarget, lerpValue);
+        camara.lookAt(new THREE.Vector3(0, 0, 0));
+    } else {
+        isAnimatingCamera = false;
+    }
+}
+function change_camera() {
+    clickCount += 1;
+    switch (clickCount) {
+        case 1:
+            cameraTarget.set(3.40, 0.672, 0.000);
+            isAnimatingCamera = true;
+            animateCamera();
+            break;
+        case 2:
+            cameraTarget.set(0.070, 0.672, -3.40);
+            isAnimatingCamera = true;
+            animateCamera();
+            break;
+        case 3:
+            cameraTarget.set(-3.40, 0.672, 0.000);
+            isAnimatingCamera = true;
+            animateCamera();
+            break;
+        case 4:
+            cameraTarget.set(0.009, 0.672, 3.40);
+            isAnimatingCamera = true;
+            animateCamera();
+            clickCount = 0;
+            break;
+    }
+}
+
 document.getElementById('colorSelector').addEventListener('change', changeColor);
 document.getElementById('sizeSelector').addEventListener('change', changeSize);
 document.getElementById('move_model_y').addEventListener('input', move_model_y);
 document.getElementById('move_model_x').addEventListener('input', move_model_x);
 document.getElementById('move_model_z').addEventListener('input', move_model_z);
+document.getElementById('btn_change_camera').addEventListener('click', change_camera);
 document.getElementById("btn_open_drawers").addEventListener('click', playGavetas);
 document.getElementById("btn_open_doors").addEventListener('click', playPortas);
 
-let delta = 0;			  // tempo desde a última atualização
-let relogio = new THREE.Clock(); // componente que obtém o delta
-let latencia_minima = 1 / 60;    // tempo mínimo entre cada atualização
+let delta = 0;
+let relogio = new THREE.Clock();
+let latencia_minima = 1 / 60;
 function animate() {
     requestAnimationFrame(animate);
     delta += relogio.getDelta();
